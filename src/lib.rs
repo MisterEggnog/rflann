79952afb0deb7ed616ba17e25a082e8a0f271eb8
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2020-08-25 15:54:44 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+
+#[repr(C, packed)]
+pub struct Vector {
+	x: i64,
+	y: i64,
+	z: i64,
+}
+
+pub struct ChunkPoints {
+	points: Vec<Vector>,
+}
+
+impl Vector {
+
+	#[inline]
+	pub fn new(x: i64, y: i64, z: i64) -> Vector {
+		Vector{x, y, z}
+	}
+
+	#[inline]
+	pub fn get_x(&self) -> i64 {
+		self.x
+	}
+
+	#[inline]
+	pub fn get_y(&self) -> i64 {
+		self.y
+	}
+
+	#[inline]
+	pub fn get_z(&self) -> i64 {
+		self.z
+	}
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
