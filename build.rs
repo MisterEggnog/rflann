bfdfec29a8da9c4e21d8edf9ad3deededf7966d9
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2020-09-29 17:37:28 -0700

    Removed license

--- build.rs
+++ build.rs
@@ -1,15 +1,9 @@
-/*
- * This file is under the BSD license.
- * See the end of the file for the full license agreement.
- */
 extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-	println!("cargo:rerun-if-changed=src/link.hpp");
-
+fn build_bindings() {
 	let bindings = bindgen::Builder::default()
 		.header("src/link.hpp")
 		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -20,32 +14,18 @@ fn main() {
 		.write_to_file(out_path.join("bindings.rs")).unwrap();
 }
 
-/*
- * Software License Agreement (BSD License)
- *
- * Copyright 2020  Baldwin, Josiah.
- *   All rights reserved.
- *
- * THE BSD LICENSE
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+fn build_lib() {
+	cc::Build::new()
+		.cpp(true)
+		.file("src/points.cpp")
+		.include("src")
+		.compile("points");
+}
+
+fn main() {
+	println!("cargo:rerun-if-changed=build.rs");
+	println!("cargo:rerun-if-changed=src/link.hpp");
+	println!("cargo:rerun-if-changed=src/points.cpp");
+	build_bindings();
+//	build_lib();
+}
--- src/points.cpp
+++ src/points.cpp
@@ -1,72 +1,7 @@
-/*
- * This file is under the BSD license.
- * See the end of the file for the full license agreement.
- */
+
 #include <cstdint>
 #include <vector>
 #include "nanoflann.hpp"
 
 using point = int64_t[3];
 using namespace nanoflann;
-
-void get_points(point* buffer, size_t length, point vec);
-
-struct point_cloud {
-	std::vector<point> buffer_;
-
-	size_t kdtree_get_point_count() const { return buffer_.size(); }
-
-	int64_t kdtree_get_pt(const size_t id, int dim) const {
-		return buffer_[id][dim];
-	}
-
-	// Not certain what this does
-	template <class BBOX>
-	bool kdtree_get_bbox(BBOX&) const { return false; }
-
-};
-
-using KDTree = KDTreeSingleIndexDynamicAdaptor<
-	L2_Simple_Adaptor<int64_t, point_cloud>,
-	point_cloud,
-	3>;
-
-void
-get_points_dynamc(const KDTree& pcloud, point vec) {
-	auto cloud = point_cloud();
-	auto index = KDTree(3, cloud, KDTreeSingleIndexAdaptorParams(10));
-}
-
-int
-main() {
-}
-
-/*
- * Software License Agreement (BSD License)
- *
- * Copyright 2020  Baldwin, Josiah.
- *   All rights reserved.
- *
- * THE BSD LICENSE
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
