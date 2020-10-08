4f8680884ab2271a17d5628f1c3751b08f76ebbe
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2020-10-07 19:57:43 -0700

    Began rust frontend

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,11 @@
 
 mod bindings;
 
-#[repr(C, packed)]
-pub struct Vector {
-	x: i64,
-	y: i64,
-	z: i64,
-}
+#[derive(Debug)]
+pub struct PointCloud();
 
-pub struct ChunkPoints {
-	points: Vec<Vector>,
-}
-
-impl Vector {
-
-	#[inline]
-	pub fn new(x: i64, y: i64, z: i64) -> Vector {
-		Vector{x, y, z}
-	}
-
-	#[inline]
-	pub fn get_x(&self) -> i64 {
-		self.x
-	}
-
-	#[inline]
-	pub fn get_y(&self) -> i64 {
-		self.y
-	}
-
-	#[inline]
-	pub fn get_z(&self) -> i64 {
-		self.z
-	}
-
-}
+pub struct PointCloudIter();
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
