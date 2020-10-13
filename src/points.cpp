c225e3052f0b5363d5fdef7cb8fa3615729857a7
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2020-10-12 20:39:20 -0700

    Added contructers/destructors for cloud

--- src/points.cpp
+++ src/points.cpp
@@ -10,6 +10,10 @@
 
 // Internal functions
 
+PointCloud::PointCloud(std::vector<Point>&& cloud):
+	PointCloudKd(3, Cloud(std::move(cloud)), KDTreeSingleIndexAdaptorParams(10)) {
+}
+
 PointBufIter::PointBufIter(std::vector<Point>::const_iterator begin, std::vector<Point>::const_iterator end) {
 	curr_ = begin;
 	end_ = end;
@@ -28,6 +32,28 @@ PointBufIter::next(int64_t& x, int64_t& y, int64_t& z) {
 }
 
 // C wrapper functions
+
+PointCloud*
+new_cloud() {
+	std::vector<Point> empty;
+	return new PointCloud(std::move(empty));
+}
+
+PointCloud*
+from_points(int64_t** array, size_t size) {
+	std::vector<Point> points(size);
+	for (size_t i = 0; i < size; i++) {
+		points[i][0] = array[i][0];
+		points[i][1] = array[i][1];
+		points[i][2] = array[i][2];
+	}
+	return new PointCloud(std::move(points));
+}
+
+void
+destroy_cloud(PointCloud* cloud) {
+	delete cloud;
+}
 
 PointCloudIter*
 get_points(const PointCloud* cloud) {
